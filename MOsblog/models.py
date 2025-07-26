9b703577d6669eb7b17a4e3d8e54ad6503808791
Rayo0131 <motunrayo188@gmail.com>
2025-07-26 13:23:16 +0100

    Mo's Blog (W.I.P)

--- MOsblog/models.py
+++ MOsblog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+    
+class Article(models.Model):
+    title=models.CharField(max_length=200)
+    image = models.ImageField(upload_to='article/', blank=True, null=True)
+    content = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True) 
+
+    def __str__(self):
+        return self.title  
